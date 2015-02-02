978b0e66accd16ead683a1d247fa198b94bcbd6b
Isernhagen, Seth <sisernha@Advent.COM>
2015-02-01 19:01:47 -0800

    adding more files

---
+++ greeter.js
@@ -0,0 +1,42 @@
+function Greeter (lang) {
+    this.language = lang;
+    
+    this.greet = function () {
+        if (this.language == 'en')
+        return "Hello World";
+    else if (this.language == 'fr')
+        return "Bonjour tout le monde";
+    else if (this.language == 'it')
+        return "Buongiorno a tutti!";
+    
+    return "We don't speak that language";
+    };
+}
+
+exports.version = "0.1.0";
+
+exports.create_greeter = function (lang) {
+    return new Greeter(lang);
+};
+
+exports.hello_world = function (lang) {
+    if (lang == 'en')
+        return "Hello World";
+    else if (lang == 'fr')
+        return "Bonjour tout le monde";
+    else if (lang == 'it')
+        return "Buongiorno a tutti!";
+    
+    return "We don't speak that language";
+};
+
+exports.goodbye = function (lang) {
+    if (lang == 'en')
+        return "Bye";
+    else if (lang == 'fr')
+        return "Au revoir";
+    else if (lang == 'it')
+        return "Ciao!";
+    
+    return "We don't speak that language booo";
+};
---
+++ test.js
@@ -0,0 +1,7 @@
+var greeter = require('./greeter');
+
+var g = greeter.create_greeter('it');
+console.log(g.greet());
+
+//console.log(greeter.hello_world("en"));
+//console.log(greeter.goodbye("fr"));
